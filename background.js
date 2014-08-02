d20956274940848a499199a88509d63f6ca0bf9a
wmichaelv <wmichaelv@hotmail.com>
2014-08-02 15:15:17 -0700

    vanilla built

---
+++ background.js
@@ -0,0 +1,9 @@
+// vanilla copy-paste
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('window.html', {
+    'bounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
+});
